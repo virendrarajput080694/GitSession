7d86ab515e73783f764eeace6c126f5ebeb17e37
Virendra Rajput <vrajput@synoptek.com>
2025-02-21 22:50:46 +0530

    American development team

--- src/test/java/stepDefinations/Hooks.java
+++ src/test/java/stepDefinations/Hooks.java
@@ -13,8 +13,6 @@ public class Hooks {
 		
 		//Execute this code only when place id is Null.
 		//Write a code that will give you place id
-		System.out.println("Hello World!!");
-		System.out.println("GM Virendra!!");
 		StepDefination sd = new StepDefination();
 		if(StepDefination.place_id==null)
 		{
